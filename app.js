b04f3e0ddea668db8c362f40265cc067d04fe417
sagar929 <sagarshrama130804@gmail.com>
2025-06-15 01:00:53 +0530

    15-6

---
+++ app.js
@@ -0,0 +1,19 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+
+
+app.set("view engine", "ejs");
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(3000);
