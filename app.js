62671f1b24c8baecd1df00cb076bc1fc82cf0d2f
sagar929 <sagarshrama130804@gmail.com>
2025-06-15 15:27:36 +0530

    login and logout created sucessfully

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const userModel = require('./models/user');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const jwt =  require('jsonwebtoken');
 
 
 app.set("view engine", "ejs");
@@ -14,6 +17,56 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.post('/create', async(req, res) => {
+    let {username, email, password, age} = req.body;
+
+   bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password, salt, async(err, hash) => {
+       // Here you would typically save the user with the hashed password
+        let createdUser = await userModel.create({
+            username,
+            email,
+            password: hash,
+            age
+        });
+
+      let token =   jwt.sign({email},"shhhhhhhhhhhhhhhhhhhsss");
+        res.cookie("token",token);
+
+
+        res.send(createdUser)
+
+      
+    })
+    })
+        
+       
+    });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+}) ; 
+
+app.post('/login', async(req, res) => {
+    // let {email, password} = req.body;
+    let user = await userModel.findOne({email: req.body.email});
+    if(!user){
+        return res.send("User not found");
+    }
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if(result){
+           res.send("Login successful");
+        }else{
+            res.send("Invalid credentials");
+        }
+    })
+});
+    
+app.get("/logout", (req,res)=>{
+    res.cookie("token","");
+    res.redirect("/");
+})
 
 
 app.listen(3000);
